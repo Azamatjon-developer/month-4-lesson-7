849ad83c3091fd38eb30b7857b4871fcf43c46b6
Azamatjon <azamatergasev01@gmail.com>
2024-03-30 00:08:00 +0500

    js tasks done

---
+++ main.js
@@ -0,0 +1,178 @@
+//  1 task Array dagi 1-va oxirgi sonlar yig'indisini toping
+
+
+// let arr = [1 , 2 , 3, 4 ,5]
+
+//  let total = arr[0] + arr[arr.length -1] 
+
+
+// console.log(`Arraydagi birinchi va oxirgi sonlar yig'indisi ${total} ga teng`) 
+
+
+
+//  1 task finished  
+
+
+//  2 task  Ixtiyoriy array yarating agar arrayning 1-elementi juft bo'lsa uni toq songa agar toq bo'lsa juft songa almashtiring 1-element random bo'lishi kerak 
+
+
+// let arr = [ parseInt(Math.random () *10) , 2,3,4,5]
+// console.log( ` ${arr[0]} random son` )
+
+// let result = arr[0] + 1
+
+// console.log(` 1 chi element random son juft bolsa toq songa toq bolsa juft songa aylanadi ${result}`)  
+
+
+//  2 task finished  
+
+
+//  3 task [5, 16, 7, 4, 10, 3, 4, 4, 14, 5, 14, 4, 44, 4] shu arraydan 4 soni olib tashlansin . 
+
+
+//     let arr = [5, 16, 7, 4, 10, 3, 4, 4, 14, 5, 14, 4, 44, 4] 
+//     let result = []
+
+
+//     for (let i = 0 ; i < arr.length ; i++) {
+
+//         if(arr[i] !== 4) {
+//             result.push(arr[i])
+//         }
+
+//     }
+
+// console.log(`Arr ni ichidagi 4 qatnashmagan sonlar ${result} dan iborat `)
+
+//  3 task finished  
+
+
+//  4 task nums = [1,3,2,4,5,6,99,102], target = 5, o'sish va kamayish tartibli array ichida target qiymati array ichida nechinchi indexda turganini aniqlang . 
+
+
+// let arr = [1,3,2,4,5,6,99,102]  
+
+// arr.sort((a,b) => a-b) 
+
+// console.log(`osish tartibida sortlangan ${arr}`)  
+
+
+// let a = 0 
+
+
+// function num_get (param) {
+//     for (let i = 0; i < arr.length ; i++) {
+
+//         if (arr[i] == 5) {
+//             a = i 
+//         }
+
+//     }
+// } 
+
+
+
+// num_get (5)
+// let b = arr.length - a -1
+// console.log(`osish tartibida ${a} ixdexda joylashgan `)
+// console.log(`kamayish tartibida ${b} ixdexda joylashgan `) 
+
+
+
+
+
+
+// 5 task  Ixtiyoriy sonlardan iborat array olinsin va uning ichidagi sonlar yig'indisi topilsin 
+
+// let arr = [1,2,6,7]
+// let result = 0 
+// for (let i = 0; i < arr.length; i++) {
+    
+//     result += arr[i]
+// }
+
+
+// console.log(`Arrayni ichidagi sonlarni yigindisi ${result} ga teng`)
+//  5 task finished 
+
+
+//  6 task starts  osish va kamayish tartibida indexlarini topadi 
+
+// let arr = [1,3,2,4,6,5,7, 67,87,99,98,100,102]   
+
+// arr.sort((a,b) => a-b) 
+
+// console.log(`osish tartibidagi sortlangan array ${arr}`) 
+
+
+// let indexA = 0 
+// let indexB = 0    
+
+
+// function getNumber (params) {
+
+//     for (let i = 0; i< arr.length; i++) {
+//         if (arr[i] == params) {
+//             indexA = i 
+
+//         }
+//     }
+// }
+
+// getNumber (2) 
+// arr.sort((a,b) => b-a ) 
+
+// console.log(`kamayish tartibidagi sortlangan array ${arr}`)
+
+// function NumberChange (number) {
+//     for (let g = 0 ; g < arr.length ; g++) {
+
+//         if(arr[g] == number) {
+//             indexB = g
+//         }
+//     }
+
+// }
+
+
+
+// NumberChange (2)
+
+
+// console.log(`osish tartibida ${indexA} indexda joylashgan `) 
+// console.log(`kamayish tartibida ${indexB} indexda joylashgan `) 
+
+
+
+// let obj = {number1 : 1, number2: 2, number3 : 3, number4 : 4} 
+
+
+
+// function addNumber (num) {
+
+// }
+
+// addNumber ()
+
+//  6 task finished  
+
+
+// 8 task   Ixtiyoriy fucntion va value lari sonlardan iborat bo'lgan object yaratilsin shu shart ekan ustoz tushunishim bo'yicha funciton yaratim va object yarattim objectni valuesi sondan yashkil topgan boldi bu masalani hato demang ustoz chunki shartini ozi tushunarsiz bolgan 
+
+
+// function num () {
+
+//     let obj ={num1: 23 , mum2: 32}
+
+
+
+// }
+
+// num ()
+
+
+//  8 task finished   
+
+
+
+
